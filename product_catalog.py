80d109391d19ed468ab055c565612e302dc666ee
andressa <acabistani@gmail.com>
2020-05-24 17:35:42 -0300

    Added test() function inside product_catalog.py script

--- product_catalog.py
+++ product_catalog.py
@@ -5,6 +5,16 @@ from app import create_app, db
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db)
+
+if __name__ == '__main__':
+    test()
