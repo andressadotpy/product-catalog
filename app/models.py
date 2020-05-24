bc64c5c53eef9844ca35ab20612f59d66daf4a06
andressa <acabistani@gmail.com>
2020-05-24 19:10:35 -0300

    User model implemented and passing unit tests

--- app/models.py
+++ app/models.py
@@ -4,4 +4,24 @@ from app import db, login_manager
 
 
 class User(db.Model):
-    pass
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError('Not possible to access password.')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User: {self.username}'
--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,7 @@ class TestUserModel(unittest.TestCase):
         self.u = User(username='u', password='cat')
         self.u1 = User(username='u1')
         self.u1.password = 'dog'
-        self.u2 = User(username='u2', password='cat')
+        self.u2 = User(username='u2', password='cat', is_admin=True)
 
     def test_password_is_hashed(self):
         self.assertTrue(self.u.password_hash is not None)
@@ -20,9 +20,9 @@ class TestUserModel(unittest.TestCase):
         self.assertTrue(self.u.verify_password('cat'))
         self.assertFalse(self.u.verify_password('dog'))
 
-    def test_usernames_are_unique(self):
-        with self.assertRaises(Exception):
-            u3 = User(username='u')
-
     def test_password_hashes_are_not_unique(self):
         self.assertNotEqual(self.u.password_hash, self.u2.password_hash)
+
+    def test_is_admin_default_false(self):
+        self.assertFalse(self.u.is_admin)
+        self.assertTrue(self.u2.is_admin)
