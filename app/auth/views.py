a4bfc95978c6202f3620a46ce455389598c3fd04
andressa <acabistani@gmail.com>
2020-05-25 08:56:51 -0300

    Changes in auth login() - if user is admin, dashboard is different than for a non-admin user

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,13 +9,13 @@ from .forms import LoginForm
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            next = request.args.get('next')
-            if next is None or not next.startswith('/'):
-                next = url_for('main.index')
-            return redirect(next)
+            login_user(user)
+            if user.is_admin:
+                return redirect(url_for('home.admin_dashboard'))
+            else:
+                return redirect(url_for('home.dashboad'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
 
